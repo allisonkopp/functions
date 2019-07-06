54e9c1673fe51fd8d00f69e9f177a739d3d2f05b
Allison Kopp <allison.kopp03@gmail.com>
2019-07-06 17:03:05 -0400

    refactor create-phone-number

--- functions/codewars/lvl-6/create-phone-number/index.js
+++ functions/codewars/lvl-6/create-phone-number/index.js
@@ -6,4 +6,15 @@ const createPhoneNumber = numbers => {
   return parsedNumber.join('');
 };
 
+//refactor
+const createPhoneNumber = numbers => {
+  let phoneNumberFormat = '(xxx) xxx-xxxx';
+  let i = 0;
+  while (phoneNumberFormat.includes('x')) {
+    phoneNumberFormat = phoneNumberFormat.replace('x', numbers[i]);
+    i++;
+  }
+  return phoneNumberFormat;
+};
+
 createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); // "(123) 456-7890"
