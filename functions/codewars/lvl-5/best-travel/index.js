3f277a131c0e63d4664c8d88a214b2b687b97f9a
Allison Kopp <allison.kopp03@gmail.com>
2019-07-07 13:33:42 -0400

    added best travel

---
+++ functions/codewars/lvl-5/best-travel/index.js
@@ -0,0 +1,9 @@
+//Need to complete this
+
+//t is distance, k is n of towns
+const chooseBestSum = (t, k, ls) => {
+  const possibleRoutes = [];
+};
+
+const ls = [50, 55, 57, 58, 60];
+chooseBestSum(174, 3, ls); //[55,58,60] 173
