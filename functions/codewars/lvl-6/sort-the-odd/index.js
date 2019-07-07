dc93ff97f73a03b4b76e8b87bb73aee8fe2700a7
Allison Kopp <allison.kopp03@gmail.com>
2019-07-07 19:23:00 -0400

    completed sort the odd

---
+++ functions/codewars/lvl-6/sort-the-odd/index.js
@@ -0,0 +1,13 @@
+const sortArray = array => {
+  const oddNumbers = [];
+  array.forEach(n => {
+    if (n % 2 === 1) oddNumbers.push(n);
+  });
+  oddNumbers.sort((a, b) => (a > b ? 1 : -1));
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] % 2 === 1) array.splice(i, 1, oddNumbers.shift());
+  }
+  return array ? array : [];
+};
+
+sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4];
