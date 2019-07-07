d8eea8c4ddf7604eba40806f4d00790f7013242f
Allison Kopp <allison.kopp03@gmail.com>
2019-07-07 00:43:02 -0400

    completed direction reduction

--- functions/codewars/lvl-5/directions-reduction/index.js
+++ functions/codewars/lvl-5/directions-reduction/index.js
@@ -1,11 +1,18 @@
-//Not complete
-
-const dirReduc = arr => {};
+const dirReduc = arr => {
+  const oppositeDirections = [{ NORTH: 'SOUTH' }, { SOUTH: 'NORTH' }, { EAST: 'WEST' }, { WEST: 'EAST' }];
+  const stack = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    key = stack[stack.length - 1];
+    const match = oppositeDirections.find(x => Object.values(x) == arr[i]);
+    match[key] ? stack.pop() : stack.push(arr[i]);
+  }
+  return stack;
+};
 
 const test1 = ['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST']; //["WEST"]
 const test2 = ['NORTH', 'WEST', 'SOUTH', 'EAST']; //["NORTH", "WEST", "SOUTH", "EAST"]
 const test3 = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'EAST', 'WEST']; //[]
 const test4 = ['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST', 'SOUTH', 'NORTH']; //["WEST"]
 
-console.log(dirReduc(test1), 'test1');
-console.log(dirReduc(test4), 'test4');
+console.log(dirReduc(test1));
+console.log(dirReduc(test4));
